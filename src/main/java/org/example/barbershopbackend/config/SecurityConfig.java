6b42de5d44a7c8ca1f0afcb0a932c0832aadb774
jafar <jaal0002@stud.kea.dk>
2026-01-05 12:50:00 +0100

    Build maven error fix

--- src/main/java/org/example/barbershopbackend/config/SecurityConfig.java
+++ src/main/java/org/example/barbershopbackend/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
+        config.addExposedHeader("Set-Cookie");
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
