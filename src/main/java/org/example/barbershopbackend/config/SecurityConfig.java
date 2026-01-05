6f620cc26e57a7f6c82e3f55e11540fe5ed40daf
jafar <jaal0002@stud.kea.dk>
2026-01-05 12:45:34 +0100

    Build maven error fix

--- src/main/java/org/example/barbershopbackend/config/SecurityConfig.java
+++ src/main/java/org/example/barbershopbackend/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
