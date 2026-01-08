91aa3ddab347753490cc0d5369adcb194f3cbd7f
jafar <jaal0002@stud.kea.dk>
2026-01-08 13:06:00 +0100

    Added Env file

--- src/main/java/org/example/barbershopbackend/BarbershopBackendApplication.java
+++ src/main/java/org/example/barbershopbackend/BarbershopBackendApplication.java
@@ -1,14 +1,19 @@
 package org.example.barbershopbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BarbershopBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(BarbershopBackendApplication.class, args);
     }
 }
